4f961d6a00e77577ebc98f31cf09bbeddb7e66ab
jacosro <yavoy122@gmail.com>
2017-10-25 12:26:07 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by jacosro on 25/10/17.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+    }
+}
